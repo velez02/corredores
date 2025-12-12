e6e8881f716a781236cf1a101d36950b9eddcbd3
velez02 <juanjosevelezmontoya02@gmail.com>
2025-12-12 16:29:10 -0500

    base bootstrap

--- src/App.jsx
+++ src/App.jsx
@@ -8,29 +8,29 @@ function App() {
 
   return (
     <>
-      <div>
+      <div className="App border bordrer-radius p-4 max-w-md mx-auto mt-10">
         <form action="">
-          <div>
+          <div className='mb-3'>
             <label htmlFor="name">Nombre</label>
             <input id="name" type="text" placeholder='Añade tu nombre (sin apellidos)' />
           </div>
-          <div>
+          <div className='mb-3'>
             <label htmlFor="last-name">Apellidos</label>
             <input id="last-name" type="text" placeholder='Añade tus apellidos' />
           </div>
-          <div>
+          <div className='mb-3'>
             <label htmlFor="age">Edad</label>
             <input id="age" type="number" placeholder='Añade tu edad' />
           </div>
-          <div>
+          <div className='mb-3'>
             <label htmlFor="email">Correo Electrónico</label>
             <input id="email" type="email" placeholder='tu@ejemplo.com' />
           </div>
-          <div>
+          <div className='mb-3'>
             <label htmlFor="description">Motivo de la comsulta</label>
             <textarea id="description" placeholder='Describe el problema' />
           </div>
-          <div>
+          <div className='mb-3'>
             <button type="submit">Enviar</button>
           </div>
         </form>
