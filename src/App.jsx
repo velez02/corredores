c8c697913bc1b2afed1d54834a8eeaebd4fb28b9
velez02 <juanjosevelezmontoya02@gmail.com>
2025-12-12 16:07:49 -0500

    guardado

--- src/App.jsx
+++ src/App.jsx
@@ -9,25 +9,34 @@ function App() {
   return (
     <>
       <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <form action="">
+          <div>
+            <label htmlFor="name">Nombre</label>
+            <input id="name" type="text" placeholder='Añade tu nombre (sin apellidos)' />
+          </div>
+          <div>
+            <label htmlFor="last-name">Apellidos</label>
+            <input id="last-name" type="text" placeholder='Añade tus apellidos' />
+          </div>
+          <div>
+            <label htmlFor="age">Edad</label>
+            <input id="age" type="number" placeholder='Añade tu edad' />
+          </div>
+          <div>
+            <label htmlFor="email">Correo Electrónico</label>
+            <input id="email" type="email" placeholder='tu@ejemplo.com' />
+          </div>
+          <div>
+            <label htmlFor="description">Motivo de la comsulta</label>
+            <textarea id="description" placeholder='Describe el problema' />
+          </div>
+          <div>
+            <select name="" id="">
+              <option value=""></option>
+            </select>
+          </div>
+        </form>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
